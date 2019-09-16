2a584dc5de3a9706fa1ac24fa896b4fc3e379963
Roman Volosatovs <rvolosatovs@riseup.net>
2019-09-16 22:46:07 +0200

    all-packages: use go 1.12 for cri-o

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22055,7 +22055,9 @@ in
 
   crispyDoom = callPackage ../games/crispy-doom { };
 
-  cri-o = callPackage ../applications/virtualization/cri-o {};
+  cri-o = callPackage ../applications/virtualization/cri-o {
+    buildGoPackage = buildGo112Package;
+  };
 
   ckan = callPackage ../games/ckan { };
 
